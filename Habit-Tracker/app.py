558ce028f66e852a5ff6030658d6bbfc2a78d00a
IPC <gkrwjaeoqkr1@gmail.com>
2021-09-14 14:07:38 +0900

    1.01

--- Habit-Tracker/app.py
+++ Habit-Tracker/app.py
@@ -13,7 +13,8 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
-client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
+#client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")\
+client = MongoClient('localhost',27017)
 db = client.dbsparta_plus_week4
 
 
